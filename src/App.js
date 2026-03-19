c7b678cd5e29857fc58f261f15b0436765e21c4d
deasana <demchuk.oksana.w@gmail.com>
2026-03-19 22:23:49 +0100

    feat: Added Mapbox map with react-map-gl

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
+  const [viewState, setViewState] = useState({
+    latitude: 50.450001,
+    longitude: 30.523333,
+    zoom: 8
+  });
+
   return (
     <div className="App">
-      <h1>Welcome to the React App!</h1>
+      <Map
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        style={{ width: '100vw', height: '100vh' }}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        projection="mercator"
+        maxPitch={0}
+      />
     </div>
   );
 }
