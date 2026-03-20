be04d345c2933c5cc7fa158c87173692f183d324
deasana <demchuk.oksana.w@gmail.com>
2026-03-20 22:23:30 +0100

    feat: added marker with info popup card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { useState } from 'react';
-import Map from 'react-map-gl';
+import React, { useState } from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import RoomIcon from '@mui/icons-material/Room';
+import StarIcon from '@mui/icons-material/Star';
+import "./App.css"
 
 function App() {
   const [viewState, setViewState] = useState({
@@ -19,7 +22,29 @@ function App() {
         mapStyle="mapbox://styles/mapbox/streets-v11"
         projection="mercator"
         maxPitch={0}
-      />
+      >
+        <Marker longitude={30.525346} latitude={50.449515} anchor="bottom">
+          <RoomIcon style={{ fontSize: viewState.zoom * 3, color: 'slateblue' }} />
+        </Marker>
+        <Popup longitude={30.525346} latitude={50.449515} anchor="left" closeButton={true} closeOnClick={false}>
+          <div className="card">
+            <h4>Eiffel Tower</h4>
+            <label>Review</label>
+            <p>Beautiful place. I like it.</p>
+            <label>Rating</label>
+            <div className="stars">
+              <StarIcon style={{ color: 'gold' }} />
+              <StarIcon style={{ color: 'gold' }} />
+              <StarIcon style={{ color: 'gold' }} />
+              <StarIcon style={{ color: 'gold' }} />
+              <StarIcon style={{ color: 'gold' }} />
+            </div>
+            <label>Information</label>
+            <p className="info">Created by <b>safak</b></p>
+            <p className="info">1 hour ago</p>
+          </div>
+        </Popup>
+      </Map>
     </div>
   );
 }
