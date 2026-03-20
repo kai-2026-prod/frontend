0e7eac3f07248d0e681dd8860bdfa2d3943bb45e
deasana <demchuk.oksana.w@gmail.com>
2026-03-20 22:31:40 +0100

    feat: made the pin actually clickable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Map, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import RoomIcon from '@mui/icons-material/Room';
 import StarIcon from '@mui/icons-material/Star';
-import "./App.css"
+import './App.css';
+
+const pin = { longitude: 30.525346, latitude: 50.449515 };
 
 function App() {
   const [viewState, setViewState] = useState({
@@ -11,6 +13,8 @@ function App() {
     longitude: 30.523333,
     zoom: 8
   });
+
+  const [showPopup, setShowPopup] = useState(false);
 
   return (
     <div className="App">
@@ -23,27 +27,41 @@ function App() {
         projection="mercator"
         maxPitch={0}
       >
-        <Marker longitude={30.525346} latitude={50.449515} anchor="bottom">
-          <RoomIcon style={{ fontSize: viewState.zoom * 3, color: 'slateblue' }} />
+        <Marker longitude={pin.longitude} latitude={pin.latitude} anchor="bottom">
+          <RoomIcon
+            style={{ fontSize: viewState.zoom * 3, color: 'slateblue', cursor: 'pointer' }}
+            onClick={() => setShowPopup(true)}
+          />
         </Marker>
-        <Popup longitude={30.525346} latitude={50.449515} anchor="left" closeButton={true} closeOnClick={false}>
-          <div className="card">
-            <h4>Eiffel Tower</h4>
-            <label>Review</label>
-            <p>Beautiful place. I like it.</p>
-            <label>Rating</label>
-            <div className="stars">
-              <StarIcon style={{ color: 'gold' }} />
-              <StarIcon style={{ color: 'gold' }} />
-              <StarIcon style={{ color: 'gold' }} />
-              <StarIcon style={{ color: 'gold' }} />
-              <StarIcon style={{ color: 'gold' }} />
+
+        {showPopup && (
+          <Popup
+            longitude={pin.longitude}
+            latitude={pin.latitude}
+            anchor="left"
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setShowPopup(false)}
+          >
+            <div className="card">
+              <label>Place</label>
+              <h4>Eiffel Tower</h4>
+              <label>Review</label>
+              <p>Beautiful place. I like it.</p>
+              <label>Rating</label>
+              <div className="stars">
+                <StarIcon style={{ color: 'gold' }} />
+                <StarIcon style={{ color: 'gold' }} />
+                <StarIcon style={{ color: 'gold' }} />
+                <StarIcon style={{ color: 'gold' }} />
+                <StarIcon style={{ color: 'gold' }} />
+              </div>
+              <label>Information</label>
+              <p className="info">Created by <b>safak</b></p>
+              <p className="info">1 hour ago</p>
             </div>
-            <label>Information</label>
-            <p className="info">Created by <b>safak</b></p>
-            <p className="info">1 hour ago</p>
-          </div>
-        </Popup>
+          </Popup>
+        )}
       </Map>
     </div>
   );
